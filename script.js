7c4dad9a158b7fab32c6971a0232bacffc2c938f
aayus7 <140505716+aayus7@users.noreply.github.com>
2024-02-18 18:45:28 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+const timerLabel = document.getElementById("timer");
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
+const millisecondsLabel = document.getElementById("milliseconds");
+const reloader = document.getElementById("ohfuck");
+
+const playBtn = document.getElementById("playpause");
+const lapBtn = document.getElementById("lapbtn");
+const resetBtn = document.getElementById("reset");
+const iPlay = document.getElementById("play-pause-id");
+
+const lap = document.getElementById("lap");
+const laplist = document.getElementById("lap-list");
+
+const imgid = document.getElementById("bgimg");
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let interval;
+
+window;
+if (window.screen.width > 320 && window.screen.width <= 390) {
+  imgid.src = "portrait.webp";
+}
+
+reloader.addEventListener("click", function () {
+  location.reload();
+});
+
+playBtn.addEventListener("click", function () {
+  if (iPlay.classList.contains("ri-play-circle-fill")) {
+    iPlay.classList.remove("ri-play-circle-fill");
+    iPlay.classList.add("ri-pause-circle-fill");
+    startTimer();
+  } else {
+    iPlay.classList.remove("ri-pause-circle-fill");
+    iPlay.classList.add("ri-play-circle-fill");
+    pauseTimer();
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  resetTimer();
+});
+
+lapBtn.addEventListener("click", function () {
+  stopTimer();
+});
+
+const updateTimer = function () {
+  milliseconds++;
+  if (milliseconds === 100) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+  }
+
+  displayTimer();
+};
+
+const displayTimer = function () {
+  millisecondsLabel.textContent = padTime(milliseconds);
+  secondsLabel.textContent = padTime(seconds);
+  minutesLabel.textContent = padTime(minutes);
+};
+
+const startTimer = function () {
+  interval = setInterval(updateTimer, 10);
+};
+
+const padTime = function (ohhh) {
+  return ohhh.toString().padStart(2, "0");
+};
+
+const pauseTimer = function () {
+  clearInterval(interval);
+};
+
+const stopTimer = function () {
+  addToLapList();
+  resetTimer();
+};
+
+const addToLapList = function () {
+  const laptime = `${padTime(minutes)}:${padTime(seconds)}:${padTime(
+    milliseconds
+  )}`;
+  console.log(laptime);
+  const maxlaps = 7;
+
+  const str = `<ul id="lap-list">Lap ${lap.childElementCount} : ${laptime}</ul>`;
+  lap.insertAdjacentHTML("beforeend", str);
+  if (lap.childElementCount >= maxlaps) {
+    alert(`You have reached the maximum number of laps!`);
+    lap.lastChild.remove();
+    resetTimer();
+    location.reload();
+  }
+};
+
+const resetTimer = function () {
+  pauseTimer();
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  displayTimer();
+  if (iPlay.classList.contains("ri-pause-circle-fill")) {
+    iPlay.classList.remove("ri-pause-circle-fill");
+    iPlay.classList.add("ri-play-circle-fill");
+  }
+};
